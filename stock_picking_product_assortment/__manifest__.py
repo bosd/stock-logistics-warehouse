OCA/stock-logistics-workflow

[IMP] stock_picking_product_assortment: use base_view_inheritance_extension module to extend product domain. Unnecessary code removed

stock_picking_product_assortment/__manifest__.py
@@ -10,6 +10,6 @@
     "maintainers": ["CarlosRoca13"],
     "license": "AGPL-3",
     "installable": True,
-    "depends": ["stock", "product_assortment"],
+    "depends": ["stock", "product_assortment", "base_view_inheritance_extension"],
     "data": ["views/stock_picking_view.xml"],
 }

bosd/stock-logistics-warehouse

stock_picking_product_assortment/__manifest__.py
@@ -10,6 +10,6 @@
     "maintainers": ["CarlosRoca13"],
     "license": "AGPL-3",
     "installable": True,
-    "depends": ["stock", "product_assortment"],
+    "depends": ["base_view_inheritance_extension", "product_assortment", "stock"],
     "data": ["views/stock_picking_view.xml"],
 }