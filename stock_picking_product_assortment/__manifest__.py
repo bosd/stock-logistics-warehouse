31a417333ea8614a831c37226c7c8107f5723ec8
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2025-02-12 19:26:40 +0100

    [IMP] stock_picking_product_assortment: pre-commit auto fixes

--- stock_picking_product_assortment/__manifest__.py
+++ stock_picking_product_assortment/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Stock Picking Product Assortment",
     "version": "18.0.1.0.0",
     "category": "Warehouse Management",
-    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "maintainers": ["CarlosRoca13"],
     "license": "AGPL-3",
